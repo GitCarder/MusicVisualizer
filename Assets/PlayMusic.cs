d1f9b5b70d5a3eadfa46e39379a894835f8d026c
DESKTOP-O6DBPU5\Lukáš <lukas.smisovsky@divrlabs.com>
2017-08-07 13:40:20 +0200

    Updated script PlayMusic

--- Assets/PlayMusic.cs
+++ Assets/PlayMusic.cs
@@ -6,14 +6,12 @@ public class PlayMusic : MonoBehaviour {
 	public AudioClip clip;
 	private AudioSource audio;
 	private bool play = false;
-
-	// Use this for initialization
+	
 	void Start () {
 		audio = GetComponent<AudioSource> ();
 		audio.clip = clip;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("p")) {			
 			if (!play) {				
